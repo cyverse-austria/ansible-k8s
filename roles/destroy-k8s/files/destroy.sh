795060d130b390ef769edba44a847657115f0f73
mojib <mojib.wali@tugraz.at>
2023-10-27 14:46:06 +0200

    destroy: apt-get

--- roles/destroy-k8s/files/destroy.sh
+++ roles/destroy-k8s/files/destroy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -x
 kubeadm reset --force
-apt remove -y kubeadm kubectl kubelet kubernetes-cni kube*
-apt autoremove -y
+apt-get remove -y kubeadm kubectl kubelet kubernetes-cni kube*
+apt-get autoremove -y
 [ -e ~/.kube ] && rm -rf ~/.kube
 [ -e /etc/kubernetes ] && rm -rf /etc/kubernetes
 [ -e /opt/cni ] && rm -rf /opt/cni
